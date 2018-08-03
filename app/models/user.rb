731e4479c7a3ead73170347f9f772ce3cd9fdfb2
Johnfelix Espinosa <johnfelix.espinosa@icloud.com>
2018-08-03 18:17:50 -0400

    Role is chosen on user registration

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ApplicationRecord
-  enum role: [:user, :instructor, :student]
+  enum role: [:instructor, :student]
   after_initialize :set_default_role, :if => :new_record?
 
   def set_default_role
-    self.role ||= :user
+    self.role ||= :student
   end
 
   # Include default devise modules. Others available are:
